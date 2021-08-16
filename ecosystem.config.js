38743d0058f90a6ed0b371e441befd3c88c9a73c
Marcel Pfeifer <marcel@wolves.ink>
2021-08-16 23:09:46 +0200

    build: :construction_worker: Auto deploy to node server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,17 @@ module.exports = {
 		{
 			name: '0to100.ink',
 			exec_mode: 'cluster',
-			instances: 'max', // Or a number of instances
-			script: './node_modules/nuxt/bin/nuxt.js',
+			instances: 'max',
+			script: './.nuxt/index.js',
 			args: 'start',
+			env: {
+				PORT: 5678,
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				PORT: 6789,
+				NODE_ENV: 'production',
+			},
 		},
 	],
 }
